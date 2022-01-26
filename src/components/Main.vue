115d23275539d543d9cfe54283252ca496a5ba0c
Frampolz <francesco.colloca@hotmail.it>
2022-01-26 15:30:44 +0100

    add product emit

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,8 @@
 <template>
   <main class="main d-flex">
-    <OurProd />
+    <OurProd :productArray="products" />
     <Service />
-    <AllProd />
+    <AllProd :productArray="products" />
     <Delivery />
     <Locations />
     <StayInTouch />
@@ -20,6 +20,48 @@ import StayInTouch from "./main-component/StayInTouch.vue";
 import Shop from "./main-component/Shop.vue";
 export default {
   name: "Main",
+  data() {
+    return {
+      products: [
+        {
+          item: "choco chip cookies",
+          image: "choco-chip-cookies",
+          category: "coockies, pastries",
+          prize: "$19.00 $39.00",
+        },
+        {
+          item: "strawberry jam cookies",
+          image: "strawberry-jam-cookies",
+          category: "coockies, pastries",
+          prize: "$24.00- $62.00",
+        },
+        {
+          item: "strawberry donut",
+          image: "strawberry-donut",
+          category: "donuts, pastries",
+          prize: "$24.00- $42.00",
+        },
+        {
+          item: "perfect macarons",
+          image: "perfect-macarons",
+          category: "macarons, pastries",
+          prize: "$18.00- $52.00",
+        },
+        {
+          item: "cookies with ice cream",
+          image: "cookies-with-ice-cream",
+          category: "cookies, pastries",
+          prize: "$20.00- $48.00",
+        },
+        {
+          item: "cherry cake",
+          image: "cherry-cake",
+          category: "cakes, pastries",
+          prize: "$30.00- $62.00",
+        },
+      ],
+    };
+  },
   components: {
     OurProd,
     Service,
